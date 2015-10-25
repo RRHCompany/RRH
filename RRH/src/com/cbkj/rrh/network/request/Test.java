bb74c45d157fc95df153a430971c816b476fda2f
sclhy1017 <sclhy1017@163.com>
2015-10-25 16:55:47 +0800

    Signed-off-by: sclhy1017 <sclhy1017@163.com>

--- RRH/src/com/cbkj/rrh/network/request/Test.java
+++ RRH/src/com/cbkj/rrh/network/request/Test.java
@@ -2,6 +2,6 @@ package com.cbkj.rrh.network.request;
 
 public class Test extends BaseRequest {
 	public static void main(String[] args){
-		System.out.println("test...");
+		System.out.println("test...aa");
 	}
 }
